6313f9941c5b75b6effe420c14ffa7d59b72e5f2
Etsub-tech <etsubamha4@gmail.com>
2025-12-31 11:18:00 +0300

    Update GuestSearchPage.tsx

--- src/pages/GuestSearchPage.tsx
+++ src/pages/GuestSearchPage.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { useTheme } from '../context/ThemeContext';
 import { Search, TrendingUp, Home, MapPin, ArrowLeft, ArrowRight, ChevronDown, Moon, Sun } from 'lucide-react';
